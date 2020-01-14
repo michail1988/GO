135151363b08c1d67374f00000d390149528d4cd
Michał Łabuda <labuda.michal.pawel@gmail.com>
2020-01-14 20:58:07 +0100

    RibbonWindow

--- Go/Views/MainWindow.xaml.cs
+++ Go/Views/MainWindow.xaml.cs
@@ -2,12 +2,14 @@
 using System.Windows;
 using System.Windows.Navigation;
 
+using System.Windows.Controls.Ribbon;
+
 namespace Go.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RibbonWindow
     {
         public MainWindow()
         {
